066ea6e0c6abdb01507de7d46d43c8253690d17d
Timothy Cevallos <timcev@gmail.com>
2017-10-05 18:23:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // Write your code in this file!
 function scuberGreetingForFeet() {
-  
+ if (feet <= 400) {
+   return `this one is on me!`;
+ }else if (1999 < feet && feet < 2500){
+   return "I will gladly take your thirty bucks.";
+ }else {
+   return "No can do.";
+ }
 }
 function ternaryCheckCity(){
-   
+
 }
 function switchOnCharmFromTip(){
-  
+
 }
